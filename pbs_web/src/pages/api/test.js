5057c181fc098ef0831c7036a7e96d808e64a665
mizan <68957804+mizan0515@users.noreply.github.com>
2024-08-04 14:21:32 +0900

    ff
    ffdas

--- pbs_web/src/pages/api/test.js
+++ pbs_web/src/pages/api/test.js
@@ -4,8 +4,4 @@ export default function handler(요청 , 응답 ){
     console.log("test");
     응답.status(200).json({ message: 'Hello from aaa.com API!' });
 }
-/*
-export default function GET(req: NextRequest) {
-    console.log("test");
-    return NextResponse.json({ message: "Hello, Next.js!" });  
-}*/
+
